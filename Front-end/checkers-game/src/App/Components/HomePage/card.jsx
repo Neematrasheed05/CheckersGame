ff5e6fc6af34b731d194a88c691b0bb2eac04b98
Neematrasheed05 <nymatrasheed05@gmail.com>
2024-07-26 13:18:40 +0300

    changed background images

--- Front-end/checkers-game/src/App/Components/HomePage/card.jsx
+++ Front-end/checkers-game/src/App/Components/HomePage/card.jsx
@@ -16,7 +16,7 @@ const Card = () => {
         console.error('Error playing audio:', error);
       });
     }
-    navigate('/register'); // Change to your desired route
+    navigate('/register'); 
 
   };
 
@@ -24,7 +24,7 @@ const Card = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
-      <div className="w-1/2 h-88 p-5 text-black bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg border border-gray-200 rounded-md flex flex-col cursor-pointer transform origin-bottom-right transition-all duration-600 ease-in-out hover:rotate-8 hover:border-transparent hover:bg-opacity-50">
+      <div className="w-1/2 h-88 p-5 text-white bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg border border-gray-200 rounded-md flex flex-col cursor-pointer transform origin-bottom-right transition-all duration-600 ease-in-out hover:rotate-8 hover:border-transparent hover:bg-opacity-50">
         <div className="flex-1">
           <p className="text-2xl my-6 font-semibold game-font animate-game-on">
           Checkers is a classic board game that comes to life online. You can play checkers versus the computer or with a friend whenever you want! Play today immediately on all of your devices using your favorite internet browser          </p>
